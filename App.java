4653e07589d41fa483c4a02ef8ef681f634c2818
Neerajaflm <niraja803@gmail.com>
2025-04-07 23:12:52 +0530

    Update App.java

---
+++ App.java
@@ -0,0 +1,17 @@
+this is java application 
+  small program like hello world!
+  class public{
+  pubic static void main(string args[]) {
+  system.out.println("hello world!");
+}
+}
+
+
+
+
+
+
+
+
+
+                         
